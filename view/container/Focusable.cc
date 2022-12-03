daeac8dff7e3c40347fab5935a23f1ad9f62e970
DrMagicalStone <45792652+DrMagicalStone@users.noreply.github.com>
2022-12-03 21:51:05 +0800

    feature - Focusable for focusing and focuses

---
+++ view/container/Focusable.cc
@@ -0,0 +1,113 @@
+#include "Focusable.h"
+
+Focusable::constructor(Focusable* peer, char placeholder)
+{
+    if (peer == NULL)
+    {
+        this->peers = new vector<Focusable*>();
+        this->onEnterFocus();
+        this->parent     = NULL;
+        this->focusLevel = 0;
+        this->onEnterFocus();
+    }
+    else
+    {
+        this->peers      = peer->peers;
+        this->parent     = peer->parent;
+        this->focusLevel = peer->focusLevel;
+        this->focused    = false;
+    }
+    this->peers->push_back(this);
+}
+
+Focusable::constructor(char placeholder, Focusable* parent)
+{
+    if ((parent != NULL))
+    {
+        this->focusLevel = parent->focusLevel + 1;
+        if (parent->currentFocusedSubFocusable == NULL)
+        {
+            parent->currentFocusedSubFocusable = this;
+        }
+    }
+    else
+    {
+        this->focusLevel = 0;
+    }
+    this->parent = parent;
+    this->peers  = new vector<Focusable*>();
+    this->onEnterFocus();
+    this->peers->push_back(this);
+}
+
+Focusable* Focusable::switchFocus(int focusLevel)
+{
+    if (focusLevel == this->focusLevel)
+    {
+        this->onExitFocus();
+
+        Focusable* currentFocused = (*this->peers)[(indexOfFirst(*(this->peers), this) + 1) % this->peers->size()];
+
+        currentFocused->onEnterFocus();
+
+        return currentFocused;
+    }
+    else
+    {
+        this->currentFocusedSubFocusable = this->currentFocusedSubFocusable->switchFocus(focusLevel);
+        return this;
+    }
+}
+
+void Focusable::onEnterFocus()
+{
+    this->focused = true;
+}
+
+void Focusable::onExitFocus()
+{
+    this->focused = false;
+}
+
+Focusable::~constructor()
+{
+    if (this->focusLevel != 0)
+    {
+        vector<Focusable*>* subFocusables = this->peers;
+
+        {
+            isize i    = 0;
+            isize size = subFocusables->size();
+
+            if (size == 1)
+            {
+                delete subFocusables;
+                this->parent->currentFocusedSubFocusable = NULL;
+            }
+            else
+            {
+                if (this->focused)
+                {
+                    this->switchFocus(this->focusLevel);
+                }
+            }
+
+            for (; i < size; i++)
+            {
+                if (this == (*subFocusables)[i])
+                {
+                    break;
+                }
+            }
+
+            i++;
+
+            for (; i < size; i++)
+            {
+                subFocusables[i - 1] = subFocusables[i];
+            }
+
+            subFocusables->pop_back();
+        }
+    }
+}
---
+++ view/container/Focusable.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "../../head.h"
+#include "ViewContainer.h"
+
+#ifdef constructor
+#undef constructor
+#endif
+
+#define constructor Focusable
+
+/**
+ * Abstract class for all views can be focused (selected).
+ * A focused view may able to do more action, for example, a focused text field accesses keyboard inputs and put them into itself.
+ * At begining, the first newed Focusable will be on focus.
+ * To swith focus to other Focusable ViewContainers, call the method "switchFocus".
+ * "switchFocus" switches the focus on the specified level to a peer of the current Focusable ViewContainer at the specified level.
+ * When a Focusable ViewContainer becomes the focus, its method "onEnterFocus" will be invoked;
+ * When a Focusable ViewContainer becomes no more the focus, its method "onExitFocus" will be invoked.
+ */
+class Focusable : public ViewContainer
+{
+  private:
+    int focusLevel;
+
+    bool focused;
+
+    vector<Focusable*>* peers;
+
+    Focusable* currentFocusedSubFocusable;
+
+    Focusable* parent;
+
+  public:
+    constructor(Focusable* peer, char placeholder);
+
+    constructor(char placeholder, Focusable* parent);
+
+    virtual Focusable* switchFocus(int focusLevel);
+
+    virtual void onEnterFocus();
+
+    virtual void onExitFocus();
+
+    ~constructor();
+};
